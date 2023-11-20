ea0a1e32a69ce9c0502cb27b92156767ade27ec3
VuLH <lehoangvu@bacoor.co>
2023-11-20 17:34:29 +0700

    fix: port env

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.use(express.json({ limit: '50mb' }))
   app.use(express.urlencoded({ limit: '50mb' }))
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3001)
+  const PORT = process.env.PORT || 3001
+  await app.listen(PORT)
 }
 bootstrap()
