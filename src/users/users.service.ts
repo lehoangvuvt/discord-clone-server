6f0ce51ef36c09d82737eb8e2707d4d5e230a779
VuLH <lehoangvu@bacoor.co>
2023-12-09 15:13:59 +0700

    investigate: url redis

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,7 +30,7 @@ export class UsersService {
     @InjectModel(Activity.name) private activityModel: Model<Activity>
   ) {
     this.publisher = createClient({
-      url: process.env.REDIS_URL,
+      url: 'redis://default:OfoEjeejfkDelMpoCHJ55I5CP2JCDBKO@discord-clone-server.railway.internal:6379',
       username: process.env.REDISUSER,
       password: process.env.REDIS_PASSWORD
     })
