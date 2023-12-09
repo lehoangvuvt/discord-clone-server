6ed2eea9a27bd312620e656d64031cf2216159cc
VuLH <lehoangvu@bacoor.co>
2023-12-09 15:11:47 +0700

    fix: redis url

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,7 +30,7 @@ export class UsersService {
     @InjectModel(Activity.name) private activityModel: Model<Activity>
   ) {
     this.publisher = createClient({
-      url: process.env.REDIS_PRIVATE_URL,
+      url: process.env.REDIS_URL,
       username: process.env.REDISUSER,
       password: process.env.REDIS_PASSWORD
     })
